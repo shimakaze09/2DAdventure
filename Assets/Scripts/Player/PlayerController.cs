b428898f87d3202726aa85068ae22fd56b6e5f2e
John <yangtianzhuo970425@gmail.com>
2023-08-09 14:19:20 +1200

    Implementing Character Movement and Flipping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,17 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed;
     private PlayerInputControl inputControl;
     private Vector2 inputDirection;
+    private Rigidbody2D rb;
 
     private void Awake()
     {
         inputControl = new PlayerInputControl();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 
     private void OnEnable()
@@ -24,8 +33,16 @@ public class PlayerController : MonoBehaviour
         inputControl.Disable();
     }
 
-    private void Update()
+    private void Move()
     {
-        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+
+        var faceDir = (int)transform.localScale.x;
+        if (inputDirection.x > 0)
+            faceDir = 1;
+        else if (inputDirection.x < 0)
+            faceDir = -1;
+        // flip character
+        transform.localScale = new Vector3(faceDir, 1, 1);
     }
 }
